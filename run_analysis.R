9ba4d63757f7ee6566c2818dd1422989f793fb3e
benrus200 <62921585+benrus200@users.noreply.github.com>
2020-05-17 09:56:23 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -22,16 +22,16 @@ subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names
 x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
-#Merges the training and the test sets to create one data set
+#Merge the training and test sets to create one data set
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 Subject <- rbind(subject_train, subject_test)
 Merged_Data <- cbind(Subject, Y, X)
 
-#Extracts only the measurements on the mean and standard deviation for each measurement
+#Extract only the measurements on the mean and standard deviation for each measurement
 TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
 
-#Uses descriptive activity names to name the activities in the data set
+#Use descriptive activity names to name the activities in the data set
 TidyData$code <- activities[TidyData$code, 2]
 
 #Appropriately labels the data set with descriptive variable names
@@ -49,7 +49,7 @@ names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRU
 names(TidyData)<-gsub("angle", "Angle", names(TidyData))
 names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
 
-#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for 
-#each activity and each subject.
+#From the data set created above, create a second, independent tidy data set with the 
+#average of each variable for each activity and each subject.
 EndData <- TidyData %>% group_by(subject, activity) %>% summarise_all(funs(mean))
 write.table(EndData, "EndData.txt", row.name=FALSE)
